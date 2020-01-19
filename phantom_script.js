e1af54f5aef031605a1dd2080ec86f569f66aca2
naveen <naveenmanchanda1995@gmail.com>
2020-01-20 03:29:47 +0530

    first commit

---
+++ phantom_script.js
@@ -0,0 +1,59 @@
+var resourceWait  = 300,
+    maxRenderWait = 10000,
+    url           = 'https://www.instagram.com/thebartown';
+
+var page          = require('webpage').create(),
+    count         = 0,
+    forcedRenderTimeout,
+    renderTimeout;
+    var initialtags = '<html><body>'
+var lasttags = '</body></html>'
+var betweentags = ""
+var items =[];
+
+page.viewportSize = { width: 1280, height : 1024 };
+
+function doRender() {
+    console.log(initialtags+betweentags+lasttags);
+    var webserver = require('webserver');
+var server = webserver.create();
+var service = server.listen(8080, function(request, response) {
+  response.statusCode = 200;
+  response.write(initialtags+betweentags+lasttags);
+  response.close();
+});
+
+}
+
+page.onResourceRequested = function (req) {
+    count += 1;
+   // console.log('> ' + req.id + ' - ' + req.url);
+    clearTimeout(renderTimeout);
+};
+
+page.onResourceReceived = function (res) {
+    if (!res.stage || res.stage === 'end') {
+        count -= 1;
+        if (JSON.stringify(res.url).match(/2885-15/)) {
+        betweentags = betweentags+'<img src=' + JSON.stringify(res.url) + '>'
+
+    }
+        
+       if (count === 0) {
+            renderTimeout = setTimeout(doRender, resourceWait);
+        }
+    }
+};
+
+page.open(url, function (status) {
+    if (status !== "success") {
+        console.log('Unable to load url');
+        phantom.exit();
+    } else {
+
+            forcedRenderTimeout = setTimeout(function () {
+            console.log(count);
+            doRender();
+        }, maxRenderWait);
+    }
+});
